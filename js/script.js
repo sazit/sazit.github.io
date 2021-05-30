8051ba25bc79984eeace00ee4666eaeca0dfdee3
Sajit Gurubacharya <sazit@Sajits-MBP.modem>
2021-05-31 00:28:55 +1000

    Added additonal files New pages for additional links added partial project details

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+window.onload = function () {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  console.log(modal);
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption\
+
+  var img = document.getElementById("passImage");
+
+  var modalImg = document.getElementById("passImageModal");
+  var captionText = document.getElementById("caption");
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+}
